c9af36efd1eddf378e27290be30619afe80ce686
Josh Stuible <shrewdlove@gmail.com>
2019-03-12 18:03:22 -0700

    Change text post image url to remove static folder so it’s load
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- pages/_post.vue
+++ pages/_post.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
     <h1>{{ post.title }}</h1>
+    <img :src="post.image" alt="">
     <div v-html="post.body" />
+    <!-- {{post}} -->
   </div>
 </template>
 
 <script>
 export default {
   async asyncData({ app, route }) {
+    const post = await app.$content('posts').get(route.path)
     return {
-      post: await app.$content('posts').get(route.path)
+      post: post,
     }
   }
 }
 </script>
+
+<style>
+img {
+  width: 500px;
+}
+</style>
