45d13f9264b4ea56c1b9defd91e1fbe020c7a4a7
Josh Stuible <shrewdlove@gmail.com>
2019-03-26 14:01:11 -0700

    Added freelance work placeholder copy
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- pages/freelance.vue
+++ pages/freelance.vue
@@ -1,6 +1,7 @@
 <template>
     <main>
         <h1>Freelance</h1>
+        <p>I am curerntly seaking employment as a web developer and designer.  Please contact me at joshstuible at gmail dot com.</p>
     </main>
 </template>
 
