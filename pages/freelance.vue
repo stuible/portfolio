7e31bfd55d784ba31fd6a965e1ec67a35c819fac
Josh Stuible <shrewdlove@gmail.com>
2019-04-10 17:14:36 -0700

    Fixed spelling error
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- pages/freelance.vue
+++ pages/freelance.vue
@@ -2,7 +2,7 @@
     <main>
         <h1>Freelance</h1>
         <br>
-        <p>I am curerntly seaking employment as a web developer and designer at an exciting startup.</p>
+        <p>I am currently seaking employment as a web developer and designer at an exciting startup.</p>
         <br><br>
         <p>Please contact me at joshstuible at gmail dot com.</p>
     </main>
