7c1d3d9653ef2fc9d86700e17142086215e8c841
Josh Stuible <shrewdlove@gmail.com>
2019-04-25 02:33:55 -0700

    Added other social media
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- components/home/Doing.vue
+++ components/home/Doing.vue
@@ -1,30 +1,39 @@
 <template>
   <div>
-      <div id="last-commit">
-          <h4>Last Commit</h4>
-          <p>{{lastCommitMessage}}</p>
-      </div>
-      <!-- <ul>
+    <section id="last-commit">
+      <h4>Last Commit</h4>
+      <p>{{lastCommitMessage}}</p>
+    </section>
+    <section>
+      <h4>Linkedin</h4>
+    </section>
+    <section>
+      <h4>Twitter</h4>
+    </section>
+    <section>
+      <h4>Instagram</h4>
+    </section>
+    <!-- <ul>
           <li v-for="commit in commits" v-bind:key="commit.id">{{commit.payload.commits[0].message}}</li>
-      </ul> -->
+    </ul>-->
   </div>
 </template>
 
 <script>
 export default {
   data() {
-      return {
-          commits: '',
-          lastCommit: undefined
-      }
+    return {
+      commits: "",
+      lastCommit: undefined
+    };
   },
   computed: {
-      lastCommitMessage(){
-          if(this.lastCommit){
-              return this.lastCommit.payload.commits[0].message;
-          }
-          else return 'Loading Last Commit...'
-      }
+    lastCommitMessage() {
+      if (this.lastCommit) {
+        const message = this.lastCommit.payload.commits[0].message.split("\n");
+        return message[0];
+      } else return "Loading Last Commit...";
+    }
   },
   mounted() {
     fetch("https://api.github.com/users/stuible/events")
@@ -32,7 +41,11 @@ export default {
         return response.json();
       })
       .then(json => {
-          const filteredEvents = json.filter(event => event.type === 'PushEvent' && event.payload.commits[0].message !== undefined);
+        const filteredEvents = json.filter(
+          event =>
+            event.type === "PushEvent" &&
+            event.payload.commits[0].message !== undefined
+        );
         this.commits = filteredEvents;
         this.lastCommit = filteredEvents[0];
       });
@@ -41,4 +54,16 @@ export default {
 </script>
 
 <style lang="scss">
+#doing {
+  section {
+    background-color: white;
+  box-shadow: 0 0 25px 9px rgba(0, 0, 0, 0.02);
+  padding: 1em;
+  margin-bottom: 1em;
+  }
+}
+#last-commit {
+  
+  // border-radius: 1em;
+}
 </style>
