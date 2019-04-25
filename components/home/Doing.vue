7b0878ebadc9e531f81bbdf644316aef8ad82587
Josh Stuible <shrewdlove@gmail.com>
2019-04-25 14:15:56 -0700

    Added icons and other social media items
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- components/home/Doing.vue
+++ components/home/Doing.vue
@@ -1,22 +1,34 @@
 <template>
-  <div>
-    <section id="last-commit">
-      <h4>Last Commit</h4>
-      <p>{{lastCommitMessage}}</p>
-    </section>
-    <section>
-      <h4>Linkedin</h4>
-    </section>
-    <section>
-      <h4>Twitter</h4>
-    </section>
-    <section>
-      <h4>Instagram</h4>
-    </section>
+  <ul>
+    <li id="last-commit">
+      <a href="https://github.com/stuible" target="_blank">
+        <img src="~static/images/social/github.svg?data" alt="Github Logo">
+        <h4>Last Commit</h4>
+        <p>{{lastCommitMessage}}</p>
+      </a>
+    </li>
+    <li>
+      <a href="https://www.linkedin.com/in/stuible/" target="_blank">
+        <img src="~static/images/social/linkedin.svg?data" alt="Github Logo">
+        <h4>Linkedin</h4>
+      </a>
+    </li>
+    <li>
+      <a href="https://twitter.com/joshstuible" target="_blank">
+        <img src="~static/images/social/twitter.svg?data" alt="Github Logo">
+        <h4>Twitter</h4>
+      </a>
+    </li>
+    <li>
+      <a href="https://www.instagram.com/stuible/" target="_blank">
+        <img src="~static/images/social/instagram.svg?data" alt="Github Logo">
+        <h4>Instagram</h4>
+      </a>
+    </li>
     <!-- <ul>
           <li v-for="commit in commits" v-bind:key="commit.id">{{commit.payload.commits[0].message}}</li>
     </ul>-->
-  </div>
+  </ul>
 </template>
 
 <script>
@@ -55,15 +67,41 @@ export default {
 
 <style lang="scss">
 #doing {
-  section {
-    background-color: white;
-  box-shadow: 0 0 25px 9px rgba(0, 0, 0, 0.02);
-  padding: 1em;
-  margin-bottom: 1em;
+  ul {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto auto;
+    grid-template-areas: ". .";
+  }
+  a {
+    display: grid;
+    grid-template-columns: 1fr 8fr;
+    grid-template-rows: auto auto;
+    align-items: center;
+    grid-template-areas: "icon ." "icon .";
+    column-gap: 1em;
+    // row-gap: 1em;
+
+    // background-color: white;
+    // box-shadow: 0 0 25px 9px rgba(0, 0, 0, 0.05);
+    padding: 1em;
+    margin-bottom: 1em;
+    transition: all 150ms linear;
+
+    &:hover {
+      background-color: white;
+      box-shadow: 0 0 25px 9px rgba(0, 0, 0, 0.05);
+    }
+  }
+
+  img {
+    grid-area: icon;
+    width: 100%;
+    border-right: 1px solid $colourMedium;
+    padding-right: 1em;
   }
 }
 #last-commit {
-  
   // border-radius: 1em;
 }
 </style>
