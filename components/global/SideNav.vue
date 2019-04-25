cb6dd239e5848ed05a60ad5ea0e74bc00084d4cc
Josh Stuible <shrewdlove@gmail.com>
2019-04-25 01:20:59 -0700

    Formatted, Smaller text in sidebar at tablet breakpoint
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- components/global/SideNav.vue
+++ components/global/SideNav.vue
@@ -23,14 +23,17 @@ export default {
       var y = window.scrollY;
       var startY = 35;
       if (y > startY) {
-        this.$refs.sidebarNav.style.display = '';
+        this.$refs.sidebarNav.style.display = "";
         var scrollPercent = (startY - y + 75) / startY;
-        this.$refs.sidebarNav.style.opacity = Math.min(1, Math.max(0, -scrollPercent));
+        this.$refs.sidebarNav.style.opacity = Math.min(
+          1,
+          Math.max(0, -scrollPercent)
+        );
       } else {
-        this.$refs.sidebarNav.style.opacity = 0; 
-        this.$refs.sidebarNav.style.display = 'none';
+        this.$refs.sidebarNav.style.opacity = 0;
+        this.$refs.sidebarNav.style.display = "none";
       }
-    },
+    }
     // setOpacity
   },
   mounted() {
@@ -49,8 +52,11 @@ export default {
   transition: opacity 200ms;
   line-height: 1.75em;
   text-transform: uppercase;
-  font-size: 13px;
+  font-size: 12px;
   letter-spacing: 0.01em;
 
+  @include breakpoint(tablet) {
+    font-size: 13px;
+  }
 }
 </style>
