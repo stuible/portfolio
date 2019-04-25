b3a48a530eaacea98671e9fc4ec268fe4b16e5ae
Josh Stuible <shrewdlove@gmail.com>
2019-04-25 15:09:33 -0700

    Added lazy loading fade in for profile image
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,19 @@
         alt="Josh Stuible's Beautiful Face"
         class="profile"
       />-->
-      <img
-        :src="profPlaceholder"
-        v-lazy="{src: home.body.image, loading: profPlaceholder, preLoad: 1 }"
-        alt="Josh Stuible's Beautiful Face"
-      >
+      <div id="profile-wrapper">
+        <div
+          id="profile-pic"
+          v-lazy:background-image="{src: home.body.image, loading: profPlaceholder, preLoad: 1 }"
+        ></div>
+
+        <!-- <img
+          :src="profPlaceholder"
+          v-lazy="{src: home.body.image, loading: profPlaceholder, preLoad: 1 }"
+          alt="Josh Stuible's Beautiful Face"
+        >-->
+      </div>
+
       <div id="titles">
         <h1 class="title">{{ home.body.title }}</h1>
         <h2 class="subtitle">{{ home.body.subtitle }}</h2>
@@ -84,7 +92,8 @@ export default {
       tech: await app.$content("tech").getAll(),
       home: await app.$content("pages").get("home"),
       highlightedTech: [],
-      profPlaceholder: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPcv33LfwAHkgMr6KeirAAAAABJRU5ErkJggg=='
+      profPlaceholder:
+        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPcv33LfwAHkgMr6KeirAAAAABJRU5ErkJggg=="
     };
   },
   computed: {
@@ -266,6 +275,52 @@ export default {
   img {
     width: 100%;
     // height: 100%;
+  }
+}
+
+#profile-pic {
+  // overflow: hidden;
+  background-color: #bab3b2;
+  position: relative;
+  background-size: cover;
+  background-position: center;
+  // width: 50%;
+  // transition: box-shadow 300ms linear, opacity 200ms linear;
+
+  width: 100%;
+  &:before {
+    content: "";
+    display: block;
+    padding-top: 100%;
+  }
+
+  &::after {
+    position: absolute;
+    background-color: #bab3b2;
+    content: "";
+    display: block;
+    // padding-bottom: 100%;
+    width: 100%;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    transition: opacity 200ms linear;
+    opacity: 0;
+  }
+
+  &[lazy="loading"] {
+    &::after {
+      opacity: 1;
+    }
+  }
+
+  &[lazy="loaded"] {
+    // filter: blur(0px);
+    // box-shadow: inset 0 0 0 1000px rgba(186, 179, 178, 0);
+    // opacity: 1;
+    &::after {
+      opacity: 0;
+    }
   }
 }
 
