ea42b1f751816f6f3e4c2fb7b44078c4216be15a
Josh Stuible <shrewdlove@gmail.com>
2019-11-12 20:25:33 -0800

    disabled more broken parts of site for now
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,14 +1,11 @@
 <template>
   <main id="resume">
     <h1>Resume</h1>
-    <!-- <a :href="'/images' + resume.body.pdf">pdf</a> -->
-    <!-- <iframe :src="'/images' + resume.body.pdf" width="100%" height="500px"></iframe> -->
-    <object width="100%" height="800" :data="'/images' + resume.body.pdf">
+
+    <!-- <object width="100%" height="800" :data="'/images' + resume.body.pdf">
       <a :href="'/images' + resume.body.pdf">Download PDF</a>
-    </object>
-    <!-- <pdf src="thing" :page="1">
-      <template slot="loading">loading content here...</template>
-    </pdf> -->
+    </object> -->
+
   </main>
 </template>
 
@@ -22,7 +19,7 @@ export default {
   },
   async asyncData({ app }) {
     return {
-      resume: await app.$content("pages").get("resume")
+      // resume: await app.$content("pages").get("resume")
     };
   }
 };
